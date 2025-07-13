ec3342bf023c54628e6634dbc0c5ca27026b3685
kimhyejeong12 <rla157626@gmail.com>
2025-07-14 01:56:06 +0900

    feat: QRcode-UI구현

--- src/pages/QR/QRCodePage.tsx
+++ src/pages/QR/QRCodePage.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import Header from "../../components/common/Header.tsx";
+import Header from "../../components/common/Header";
 import imageIcon from "../../assets/QRImg.svg";
+import BottomFixedWrapper from "../../components/common/BottomFixedWrapper";
+import Button from "../../components/common/Button/Button";
+import { useNavigate } from "react-router-dom";
 
 const QRCodePage: React.FC = () => {
-    console.log("QRCodePage");
+    const navigate = useNavigate();
 
     return (
-        <div className="min-h-screen bg-white w-full mx-auto">
-            {/* 헤더: 상단 패딩을 늘려 더 아래로 */}
+        <div className="min-h-screen bg-white w-full relative flex flex-col">
             <div className="pt-10">
                 <Header
+                    onBack={() => navigate(-1)}
                     center={
                         <h1 className="text-xl font-bold text-gray-800">
                             가게 이름
@@ -17,10 +20,21 @@ const QRCodePage: React.FC = () => {
                     }
                 />
             </div>
-            {/* 헤더와 QR코드 사이 충분한 여백, QR 이미지는 수평 중앙 정렬 */}
-            <div className="mt-32 flex justify-center">
-                <img src={imageIcon} alt="QR 코드" className="w-80 h-80" />
+            <div className="flex-1 flex flex-col justify-start px-6" style={{ marginTop: "56px" }}>
+                <div className="w-full max-w-[400px] mx-auto flex flex-col items-center mt-8">
+                    <div className="text-xl font-bold text-gray-800 text-center leading-snug mb-8">
+                        QR을 캡쳐하거나<br />저장해주세요!
+                    </div>
+                    <div className="flex justify-center mb-8 w-full">
+                        <img src={imageIcon} alt="가게 QR 코드" className="w-64 h-64 mx-auto" />
+                    </div>
+                </div>
             </div>
+            <BottomFixedWrapper>
+                <Button variant="primary" className="w-[333px] h-[59px] text-lg font-bold rounded-full mx-auto">
+                    QR 저장하기
+                </Button>
+            </BottomFixedWrapper>
         </div>
     );
 };
